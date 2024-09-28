ac05e2126152ba00b8f744608e8ef11a37563bce
meleshko.a <meleshko.a@dbbest.com>
2024-09-28 21:40:05 +0200

    Cloud App A Spring with Eureka, Server Config and Load Balancing

---
+++ src/main/java/com/saloed/cloud/cloudapp/HelloController.java
@@ -0,0 +1,45 @@
+package com.saloed.cloud.cloudapp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RefreshScope
+@RestController
+public class HelloController {
+
+    public static final String CLOUD_APP_B = "cloud-app-b";
+
+    @Value("${message:Hello, default!}")
+    private String message;
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return message;
+    }
+
+    private final DiscoveryClient discoveryClient;
+    private final RestTemplate restTemplate;
+
+    public HelloController(DiscoveryClient discoveryClient, RestTemplate restTemplate) {
+        this.discoveryClient = discoveryClient;
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/fetch-data")
+    public String fetchDataFromServiceB() {
+        // Получаем список инстансов service-b
+        String serviceUrl = discoveryClient.getInstances(CLOUD_APP_B)
+            .stream()
+            .findFirst()
+            .map(si -> si.getUri().toString())
+            .orElseThrow(() -> new RuntimeException("Service % not found!".formatted(CLOUD_APP_B)));
+
+        // Отправляем запрос к service-b
+        String data = restTemplate.getForObject(serviceUrl + "/data", String.class);
+        return "Received from " + CLOUD_APP_B + ": " + data;
+    }
+}
